71758291010b974b2a6079096f868e790d618c38
Kenan7 <kenankzmv@gmail.com>
2020-05-19 01:20:36 +0400

    Add Navbar component, both component and view Product custom scss styles

---
+++ src/components/Products.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="columns is-multiline">
+    <div
+      v-for="product in products"
+      :key="product.id"
+      class="column is-2 is-4-mobile"
+    >
+      <div class="card">
+        <div class="card-image">
+          <figure class="image">
+            <img :src="product.image" alt="Placeholder image" />
+          </figure>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">{{ product.title }}</a>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["products"]),
+  },
+};
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section">
+    <div v-if="isLoaded" class="container">
+      <Products />
+    </div>
+
+    <div v-else>
+      <h1>heyyy! data could not load</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Products from "@/components/Products";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    Products,
+  },
+  computed: {
+    ...mapState(["products"]),
+    isLoaded() {
+      if (this.products.length >= 1) {
+        console.log(
+          "%cReset isLoaded",
+          "font-weight: bold; color: white; background: green;"
+        );
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  mounted() {
+    console.log(
+      "%cMOUNTED REPOS",
+      "font-weight: bold; color: white; background: blue;"
+    );
+    console.timeLog();
+  },
+  updated() {
+    console.log(
+      "%cREPOS UPDATED",
+      "font-weight: bold; color: white; background: blue;"
+    );
+    console.timeEnd();
+  },
+};
+</script>
