d800f39ec97163c2615577bb3b7f8dfe7bdc2a77
Kenan7 <kenankzmv@gmail.com>
2020-05-31 14:24:36 +0400

    fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,10 +4,10 @@ from main import views as main_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
